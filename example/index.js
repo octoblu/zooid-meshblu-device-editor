f5648ae556d756c836ae21ff567a00dff4a785d7
Olu Ayandosu <oayandosu@gmail.com>
2016-02-29 14:08:21 -0700

    It works!

--- example/index.js
+++ example/index.js
@@ -4,7 +4,12 @@ import MeshbluDeviceEditor from '../index'
 
 class Example extends Component {
   render() {
-    return <MeshbluDeviceEditor />
+    return (
+      <MeshbluDeviceEditor
+        uuid="3027e07d-87d5-4dde-b28d-65ac7a810099"
+        token="b69508202c97c59f6c76f417cba8167aa1010c76"
+      />
+    )
   }
 }
 
--- index.js
+++ index.js
@@ -2,7 +2,85 @@ import React, { Component, PropTypes } from 'react'
 import ReactSchemaForm from 'react-jsonschema-form'
 
 export default class MeshbluDeviceEditor extends Component {
+  state = {
+    device: null,
+    loading: false,
+    error: null,
+  }
+
+  static propTypes = {
+    uuid: PropTypes.string.isRequired,
+    token: PropTypes.string.isRequired,
+    server: PropTypes.string,
+    port: PropTypes.number
+  }
+
+  static defaultProps = {
+    server: 'meshblu.octoblu.com',
+    port: 443
+  }
+
+  componentDidMount() {
+    const { uuid, token, server, port } = this.props
+    const meshbluOptions = { uuid, token, server, port }
+
+    this.setState({ loading: true })
+
+    this.conn = meshblu.createConnection(meshbluOptions)
+
+    this.conn.on('ready', (data) => {
+      console.log('DEVICE AUTHENTICATED', meshbluOptions)
+
+      this.conn.whoami({}, (device) => {
+        console.log('DEVICE', device, this)
+        const { name, optionsSchema, options } = device
+        optionsSchema.title = name
+        this.setState({
+          device,
+          loading: false
+        })
+      })
+
+      this.conn.on('disconnect', function(data){
+        console.log('DISCONNECTED FROM SKYNET');
+      })
+    })
+
+    this.conn.on('notReady', (err, data) => {
+      console.log('DEVICE AUTHENTICATION FAILED', meshbluOptions)
+      this.setState({
+        error: new Error(err.message),
+        loading: false
+      })
+    })
+  }
+
+  handleSubmit = ({ formData }) => {
+    this.conn.update({ options: formData }, (data) => {
+      console.log('DEVICE UPDATED', data)
+    })
+  }
+
   render() {
-    return <h1>Meshblu Devie Editor</h1>
+    const { device, error, loading }    = this.state
+    const { uuid, token, server, port } = this.props
+
+    if (!uuid) return <div>Device UUID is required</div>
+    if (!token) return <div>Device Token is required</div>
+
+    if (loading) return <div>Loading...</div>
+    if (error) return <div>{error.message}</div>
+
+    if (!device) return <div>Device not found</div>
+
+    const { options, optionsSchema } = device
+
+    return (
+      <ReactSchemaForm
+        schema={optionsSchema}
+        formData={options}
+        onSubmit={this.handleSubmit}
+      />
+    )
   }
 }
