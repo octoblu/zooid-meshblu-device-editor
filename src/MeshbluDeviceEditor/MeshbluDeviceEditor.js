13d338835e155f32e6cdf80440be1988f411d498
Sqrt of Saturn <sqrt@octoblu.com>
2016-04-19 09:59:21 -0700

    Clean up validation and errors

--- src/MeshbluDeviceEditor/MeshbluDeviceEditor.js
+++ src/MeshbluDeviceEditor/MeshbluDeviceEditor.js
@@ -21,10 +21,10 @@ export default class MeshbluDeviceEditor extends Component {
   }
 
   componentWillMount() {
-    const schemaValidation = this.validateSchema();
+    const error = this.validateSchema();
 
-    if (_.isError(schemaValidation)) {
-      this.setState({ error: schemaValidation });
+    if (error) {
+      this.setState({ error });
     } else {
       const schemas = this.props.device.schemas['v1.0.0'];
 
@@ -47,11 +47,12 @@ export default class MeshbluDeviceEditor extends Component {
     if (!device.schemas['v1.0.0']) return new Error(UNSUPPORTED_SCHEMA_MSG);
     if (_.isEmpty(device.schemas['v1.0.0'])) return new Error(NO_SCHEMA_MSG);
 
-    return true;
+    return null;
   }
 
   handleSchemaSelection(selectedSchema) {
     const { schemas } = this.state;
+
     this.setState({
       selectedSchema: schemas[selectedSchema],
     });
@@ -59,6 +60,7 @@ export default class MeshbluDeviceEditor extends Component {
 
   render() {
     const { error, schemas, selectedSchema } = this.state;
+
     if (error) return <div>{error.message}</div>;
 
     return (
