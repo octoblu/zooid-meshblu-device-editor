29a0ebb3979c991bef1ec84ca93c8ad8dc9b3330
Sqrt of Saturn <sqrt@octoblu.com>
2016-04-28 11:54:10 -0700

    v5.0.0 added support for passing in the device

--- src/SchemaContainer/SchemaContainer.js
+++ src/SchemaContainer/SchemaContainer.js
@@ -3,28 +3,29 @@ import React, { PropTypes } from 'react';
 import ReactSchemaForm from 'react-jsonschema-form';
 
 const propTypes = {
+  device: PropTypes.object.isRequired,
   schema: PropTypes.object.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
 
 const defaultProps = {
+  device: {},
   schema: {},
   onSubmit: _.noop,
 };
 
 
-const SchemaContainer = ({ schema, onSubmit }) => {
+const SchemaContainer = ({ device, schema, onSubmit }) => {
   const handleSubmit = (form) => {
     const { formData, idSchema } = form;
     const filteredFormData = _.pick(formData, _.keys(idSchema));
     onSubmit(filteredFormData);
   };
-
   return (
     <div className="SchemaForm--configuration">
       <ReactSchemaForm
         schema={schema}
-        formData={null}
+        formData={device}
         onSubmit={handleSubmit}
       />
     </div>
